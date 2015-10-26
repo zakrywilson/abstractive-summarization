78b6d3fcd1b53531bca149e8a48da7f6a5cbb13f
zakrywilson <zakrywilson@gmail.com>
2015-10-25 20:58:09 -0600

    Added support for file IO & prints all triples

--- Triples.java
+++ Triples.java
@@ -13,52 +13,59 @@ import edu.stanford.nlp.sequences.ColumnDocumentReaderAndWriter;
 
 import edu.stanford.nlp.ie.*;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.Properties;
-import java.lang.StringBuilder;
+import java.util.*;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.IOException;
 
+
 public class Triples {
 
-  /**
-   *  Reads in the document, extracts the triples, and prints them
+  /*
+   * This map is what we're storing all the extracted information in. It's
+   * key is the sentence and it's value is a string list containing all
+   * the triples produced from the given sentence.
    */
-  public static void main(String[] args) throws Exception {
-    String document = "tolstoy.txt";
-    String text = getText(document);
-    boolean success = getTriples(text);
+  private Map<String, List<String>> bsus = null;
+  private String inputFile = null;
 
-    if (success) 
-      System.out.println("done."); 
-    else 
-      System.out.println("failure.");
+
+  protected Triples(String document, boolean writeToFile) {
+    this.inputFile = document;                // Set original file
+    String text = getText(document);          // Read and store file
+    boolean success = getTriples(text);       // Extract the triples
+    if (success) { 
+      printTriples(writeToFile);
+      done();
+    }
+    else failure();
   }
 
-  /**
-   *  Opens the file, reads the text, and returns it in a String
-   */
+
+  private void doCoreferenceSubstitution(String document) {
+    System.out.println("inside doCoreferenceSubstitution()");
+  }
+
+
   private static String getText(String document) {
     File file = new File(document);
-    StringBuilder text = new StringBuilder();
+    String text = null;
 
     try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-      text.append(reader.readLine());
-    } catch (IOException ioe) {
+      text = reader.readLine();
+    } catch (IOException e) {
       System.out.println("ERROR: unable to read file: " + document);
     }
-
-    return text.toString();
+    return text;
   }
 
-  /**
-   *  Extracts triples from text and prints them in [a | b | c] format
-   */
-  private static boolean getTriples(String text) {
 
+  private boolean getTriples(String text) {
+
+    // If something went wrong with getting the text, stop processing
     if (text == null)
       return false;
 
@@ -66,29 +73,108 @@ public class Triples {
     Properties props = new Properties();
 
     props.setProperty("annotators", 
-                      "tokenize, ssplit, pos, parse, depparse, lemma, " + 
-                      "ner, dcoref, natlog, openie");
+                      "tokenize, ssplit, pos, parse, depparse, " + 
+                      "lemma, ner, dcoref, natlog, openie");
+                      // "lemma, natlog, openie");
     StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
 
     Annotation doc = new Annotation(text);
     pipeline.annotate(doc);
 
+    // Initialize the Hashmap for storing triples
+    bsus = new HashMap<String, List<String>>();
+
     // Loop over sentences in the document
+    System.out.println("Extracting triples...");
     for (CoreMap sent : doc.get(CoreAnnotations.SentencesAnnotation.class)) {
 
       // Get the OpenIE triples for the sentence
       Collection<RelationTriple> triples = 
         sent.get(NaturalLogicAnnotations.RelationTriplesAnnotation.class);
 
+      // Using the clause splitter
+      List<SentenceFragment> clauses = 
+        new OpenIE(props).clausesInSentence(sent);
+
+      // This is where we'll store the extracted triples
+      List<String> valueBSU = new ArrayList<>();
+
       // Print the triples
-      System.out.println("Printing triples...");
+      String keySentence = clauses.toString();
       for (RelationTriple triple : triples) {
-        System.out.println( /* triple.confidence + "" + */ "[" + 
-            triple.subjectLemmaGloss() + " ; " +
-            triple.relationLemmaGloss() + " ; " +
-            triple.objectLemmaGloss() + " ]" );
+        valueBSU.add("[" + triple.subjectLemmaGloss()  + " | " +
+                           triple.relationLemmaGloss() + " | " +
+                           triple.objectLemmaGloss()   + "]");
+      }
+      this.bsus.put(keySentence, valueBSU);
+    }
+    System.out.println("\ntriples successfully extracted.");
+    return true;
+  }
+
+
+  private void printTriples(boolean writeToFile) {
+    for (Map.Entry<String, List<String>> pair : this.bsus.entrySet()) {
+      System.out.println("\n\nSENTENCE: " + pair.getKey());
+      List<String> triples = pair.getValue();
+      for (String triple : triples) {
+        System.out.println("\tBSU: " + triple);
       }
     }
-    return true;
+    if (writeToFile) writeToFile();
+  }
+
+
+  private void writeToFile() {
+    
+    if (inputFile == null || !inputFile.endsWith(".txt")) {
+      System.out.println("ERROR: invalid file."); 
+      return;
+    }
+
+    BufferedWriter writer = null;
+
+    try {
+
+      File file = new File(inputFile.replace(".txt", "-bsu.txt"));
+      if (file.exists()) file.delete();
+      file.createNewFile();
+      writer = new BufferedWriter(new FileWriter(file.getAbsoluteFile()));
+      
+      for (Map.Entry<String, List<String>> pair : this.bsus.entrySet()) {
+        writer.write("SENTENCE: " + pair.getKey()); 
+        writer.newLine();
+        List<String> triples = pair.getValue();
+        for (String triple : triples) {
+          writer.write("     BSU: " + triple);
+          writer.newLine();
+        }
+        writer.newLine();
+      }
+
+    } catch (IOException e) {
+      System.out.println("ERROR: caugh one of those cool IOExceptions.");
+    } finally {
+      if (writer != null) {
+        try { writer.close(); } catch (IOException e) { /* who cares? */ }
+      }
+    }
+  }
+
+
+  private void done() {
+    System.out.println("done.");
+  }
+
+
+  private void failure() {
+    System.out.println("failure.");
+  }
+
+
+  public static void main(String[] args) throws Exception {
+    String document = "tolstoy.txt";
+    boolean writeToFile = true;
+    Triples triples = new Triples(document, writeToFile);
   }
 }
