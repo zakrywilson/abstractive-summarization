2358bf27730547f66b1629dcdc7a2baed9d4be80
zakrywilson <zakrywilson@gmail.com>
2015-10-26 20:28:12 -0600

    Cleaned up file and added comments

--- Triples.java
+++ Triples.java
@@ -49,7 +49,16 @@ public class Triples {
    * the triples produced from the given sentence.
    */
   private Map<String, List<String>> bsus = null;
+
+  /**
+   * This map contains the named entiy recognition information by the word
+   * being the key and the associated NER tag being the value.
+   */
   private Map<String, String> ner = null;
+
+  /**
+   * The input file containing the text to be processed.
+   */
   private String inputFile = null;
 
 
@@ -70,10 +79,9 @@ public class Triples {
   * @param writeToFile - determines if output will be written to file
   */
   protected Triples(String document, boolean writeToFile) {
-    this.inputFile = document;                // Set original file
-    String text = getText(document);          // Read and store file
-    doCoreferenceSubstitution(text);          // Do coreference substitution
-    boolean success = getTriples(text);       // Extract the triples
+    this.inputFile = document;                 // Set original file
+    String text = getText(document);           // Read and store file
+    boolean success = processText(text);       // Extract the triples
     if (success) { 
       printTriples(writeToFile);
       done();
@@ -85,7 +93,7 @@ public class Triples {
  /**
   * Reads text from input file and returns it
   * @param document
-  * @return text
+  * @return text - if there was an error, text will be equal to null
   */
   private static String getText(String document) {
     File file = new File(document);
@@ -96,38 +104,34 @@ public class Triples {
     } catch (IOException e) {
       System.out.println("ERROR: unable to read file: " + document);
     }
+
     return text;
   }
 
 
- /**
-  * Performs coreference resolution and substitution
-  * @param text
-  */
-  private void doCoreferenceSubstitution(String text) {
-    System.out.println("inside doCoreferenceSubstitution()");
-  }
-
-
- /**
-  * Extracts triples from text, prints them to standard out, and (optionally)
-  * writes them back to file
+ /** 
+  * Extracts triples and NER, stores the information in seperate maps, prints
+  * the information to standard out and (optionally) writes data to file.
   * @param text
   * @return true - if processing was successful
   */
-  private boolean getTriples(String text) {
+  private boolean processText(String text) {
 
-    // If something went wrong with getting the text, stop processing
-    if (text == null)
+    // Ensure there is text to process
+    if (text == null) {
+      System.out.println("ERROR: text was null.");
       return false;
+    }
+
+    //////////////////////////////////////////////////////////////////////////
+    // CORENLP PIPELINE //////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////
 
     // Create the Stanford CoreNLP pipeline
     Properties props = new Properties();
     props.setProperty("annotators", 
                       "tokenize, ssplit, pos, parse, depparse, " + 
                       "lemma, ner, dcoref, natlog, openie");
-                      // "tokenize, ssplit, pos, depparse, 
-                      // lemma, natlog, openie");
     StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
     Annotation doc = new Annotation(text);
     pipeline.annotate(doc);
@@ -138,7 +142,13 @@ public class Triples {
 
     // Create link graph for coreference resolution
     // Map<Integer, CorefChain> graph = doc.get(CorefChainAnnotation.class);
-    // graph.getMentionsInTextualOrder();
+    // Collection<CorefChain> corefChains = graph.values();
+    // for (CorefChain corefChain : corefChains) {
+    //   //System.out.println(corefChain.getMentionsWithSameHead().toString());
+    //   System.out.println(corefChain.toString());
+    // }
+    // graph.toString();
+    // System.out.println("GRAPH:\n" + graph);
 
     //////////////////////////////////////////////////////////////////////////
     // EXTRACTING TRIPLES & NAMED ENTITY RECOGNITION /////////////////////////
@@ -151,19 +161,20 @@ public class Triples {
     // Loop over sentences in the document
     for (CoreMap sent : doc.get(CoreAnnotations.SentencesAnnotation.class)) {
 
-      // traversing the words in the current sentence
-      // a CoreLabel is a CoreMap with additional token-specific methods
+      // Traversing the words in the current sentence
       for (CoreLabel token: sent.get(TokensAnnotation.class)) {
-        // this is the text of the token
+        // Text of the token
         String word = token.get(TextAnnotation.class);
-        // this is the POS tag of the token
+        // POS tag of the token
         String pos = token.get(PartOfSpeechAnnotation.class);
-        // this is the NER label of the token
+        // NER label of the token
         String ne = token.get(NamedEntityTagAnnotation.class);
-        if ((ne.length() != 1) && !word.equals(",")) ner.put(word, ne);
+        // Store NER if NER exists (also omitting ',' -> date error)
+        if ((ne.length() != 1) && !word.equals(",")) 
+          this.ner.put(word, ne);
       }
 
-      // this is the parse tree of the current sentence
+      // This is the parse tree of the current sentence
       // Tree tree = sent.get(TreeAnnotation.class);
 
       // Get the OpenIE triples for the sentence
@@ -184,8 +195,12 @@ public class Triples {
                             triple.relationGloss() + " | " +
                             triple.objectGloss()   + "]");
       }
+
+      // Store sentences and associated BSUs inside map
       this.bsus.put(keySentence, valueBSUs);
-    }
+
+    } // Finished extracting information
+
     System.out.print("\ninformation extracted");
     return true;
   }
@@ -196,6 +211,7 @@ public class Triples {
   * @param writeToFile - determinds whether data should be written to file
   */
   private void printTriples(boolean writeToFile) {
+
     // Print out sentences and BSUs
     for (Map.Entry<String, List<String>> pair : this.bsus.entrySet()) {
       System.out.println("\n\nSENTENCE: " + pair.getKey());
@@ -204,10 +220,12 @@ public class Triples {
         System.out.println("\tBSU: " + triple);
       }
     }
+
     // Print out NER
     for (Map.Entry<String, String> pair : this.ner.entrySet()) {
       System.out.println("NER: " + pair.getKey() + " -> " + pair.getValue());
     }
+
     // Write this all to file
     if (writeToFile) writeToFile();
   }
@@ -218,6 +236,7 @@ public class Triples {
   */
   private void writeToFile() {
     
+    // Check if the input file is valid
     if (inputFile == null || !inputFile.endsWith(".txt")) {
       System.out.println("ERROR: invalid file."); 
       return;
@@ -227,6 +246,7 @@ public class Triples {
 
     try {
 
+      // Create new file to write to
       File file = new File(inputFile.replace(".txt", "-bsu.txt"));
       if (file.exists()) file.delete();
       file.createNewFile();
@@ -246,18 +266,19 @@ public class Triples {
 
       // Write NER information to file
       for (Map.Entry<String, String> pair : this.ner.entrySet()) {
-        System.out.println("NER: " + pair.getKey() + " -> " + pair.getValue());
         writer.write("NER: " + pair.getKey() + " -> " + pair.getValue());
         writer.newLine();
       }
 
     } catch (IOException e) {
       System.out.println("ERROR: unable to write to file."); return;
+
     } finally {
       if (writer != null) {
         try { writer.close(); } catch (IOException e) { /* who cares? */ }
       }
     }
+
     System.out.println("\ninformation written to file");
   }
 
@@ -284,7 +305,7 @@ public class Triples {
   * written to file
   */
   public static void main(String[] args) throws Exception {
-    String document = "tolstoy.txt";
+    String document = "cat.txt";
     boolean writeToFile = true;
     Triples triples = new Triples(document, writeToFile);
   }
