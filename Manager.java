6c7d79680929e734bbf22d8c410c1f25c61a8647
zakrywilson <zakrywilson@gmail.com>
2015-12-13 20:40:20 -0700

    Reverting back to sample text file

--- Manager.java
+++ Manager.java
@@ -14,7 +14,7 @@ public class Manager {
   public static void main(String[] args) {
 
     // Options
-    String file = "compressed-text.txt";
+    String file = "sample.txt";
     boolean writeToFile = true;
 
     // Running program
