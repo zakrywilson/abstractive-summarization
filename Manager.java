ec2e1f044b79c3a54762217ba1ae6a26267a0574
zakrywilson <zakrywilson@gmail.com>
2015-12-15 21:18:39 -0700

    Switching back to sample.txt

--- Manager.java
+++ Manager.java
@@ -14,7 +14,7 @@ public class Manager {
   public static void main(String[] args) {
 
     // Options
-    String file = "news-fountain.txt";
+    String file = "sample.txt";
     boolean writeToFile = true;
 
     // Running program
