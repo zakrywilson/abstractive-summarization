87fc1e12c67f31e5a8e6e0f26731b9b6baa41db3
zakrywilson <zakrywilson@gmail.com>
2016-01-16 15:53:29 -0700

    Decreasing visibility of code

--- Entity.java
+++ Entity.java
@@ -16,25 +16,25 @@ class Entity {
    * @param type - the type of named entity
    * @param sentenceNumber - the sentence number in which the NER was found
    */
-  protected Entity(final String noun, final String type, final int sentenceNumber) {
+  Entity(final String noun, final String type, final int sentenceNumber) {
     this.noun = noun;
     this.type = type;
-    this.sentenceNumbers = new ArrayList<Integer>(1);
+    this.sentenceNumbers = new ArrayList<>(1);
     this.sentenceNumbers.add(sentenceNumber);
   }
 
 
-  protected String getNoun() {
+  String getNoun() {
     return this.noun;
   }
 
 
-  protected String getType() {
+  String getType() {
     return this.type;
   }
 
 
-  protected List<Integer> getSentenceNumbers() {
+  List<Integer> getSentenceNumbers() {
     return this.sentenceNumbers;
   }
 }
